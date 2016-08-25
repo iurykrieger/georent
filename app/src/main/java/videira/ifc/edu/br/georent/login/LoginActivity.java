75e133e73085a58433184318d4fa353acb059170
Iury Krieger <iurykrieger96@gmail.com>
2016-08-25 20:46:28 -0300

    Terminando layout login e loading screen

--- app/src/main/java/videira/ifc/edu/br/georent/login/LoginActivity.java
+++ app/src/main/java/videira/ifc/edu/br/georent/login/LoginActivity.java
@@ -1,8 +1,10 @@
 package videira.ifc.edu.br.georent.login;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -15,5 +17,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        /* Set TextView logo font */
+        final EditText etEmail = (EditText) findViewById(R.id.et_email);
+        final EditText etPassword = (EditText) findViewById(R.id.et_password);
+        final TextView tvLogo = (TextView) findViewById(R.id.tv_logo);
+
+        Typeface typeFace = Typeface.createFromAsset(getAssets(),"fonts/source-sans-pro.regular.ttf");
+        etEmail.setTypeface(typeFace);
+        etPassword.setTypeface(typeFace);
+        tvLogo.setTypeface(typeFace);
     }
 }
