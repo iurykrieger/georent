fd8afc2482cfb28db866e691aeb42c5bfda95499
Jose Luiz Bermudez <Jose Luiz Bermudez>
2016-09-06 19:53:03 -0300

    CollapsingToolbar no cadastro de usuario

---
+++ app/src/main/java/videira/ifc/edu/br/georent/activities/UserRegisterActivity.java
@@ -0,0 +1,13 @@
+package videira.ifc.edu.br.georent.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+    }
+}
