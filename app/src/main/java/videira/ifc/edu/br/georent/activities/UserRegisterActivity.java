2e6bde5a7794905a32f538d75733d8b92356c60e
Iury Krieger <iurykrieger96@gmail.com>
2016-09-16 09:25:39 -0300

    AppBat e Layout UserCreate

--- app/src/main/java/videira/ifc/edu/br/georent/activities/UserRegisterActivity.java
+++ app/src/main/java/videira/ifc/edu/br/georent/activities/UserRegisterActivity.java
@@ -3,12 +3,16 @@ package videira.ifc.edu.br.georent.activities;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import videira.ifc.edu.br.georent.R;
 
 public class UserRegisterActivity extends AppCompatActivity {
 
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,23 @@ public class UserRegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_register);
 
         mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        mCollapsingToolbarLayout.setTitle("Adicionar Foto");
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //mCollapsingToolbarLayout.setTitle("Adicionar Foto");
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //mCollapsingToolbarLayout.setTitleEnabled(false);
+    }
+
+    /**
+     * Cria o menu com base no layout do menu_main
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
