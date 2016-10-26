5565b850929092fab0be28154985e6fe9d59c40c
Jose Luiz Bermudez <Jose Luiz Bermudez>
2016-10-26 15:56:52 -0200

    CardView no cadastro / bitmap na imagem

--- app/src/main/java/videira/ifc/edu/br/georent/activities/UserRegisterActivity.java
+++ app/src/main/java/videira/ifc/edu/br/georent/activities/UserRegisterActivity.java
@@ -1,30 +1,37 @@
 package videira.ifc.edu.br.georent.activities;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Gallery;
 import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+
 import videira.ifc.edu.br.georent.R;
 import videira.ifc.edu.br.georent.adapters.ImageAdapter;
 
 public class UserRegisterActivity extends AppCompatActivity {
 
-    final Integer[] imgs = {R.drawable.new_image, R.drawable.photo2, R.drawable.user};
+    private Integer[] imgs = {R.drawable.new_image, R.drawable.photo2, R.drawable.user};
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
     private Toolbar mToolbar;
-    private ImageSwitcher mswitcher;
-    private FloatingActionButton mFButton;
-    private Uri imageCaptureUri;
-    private SeekBar mSeekBar;
-    private TextView mTvDistance;
-
+    private static final int SELECT_PHOTO = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +44,9 @@ public class UserRegisterActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        /*     CHANGE DISTANCE    */
         final SeekBar skDistance = (SeekBar) findViewById(R.id.sb_distance);
-        final TextView tvDistance = (TextView) findViewById(R.id.tv_distance);
-
+        final TextView tvDistance = (TextView) findViewById(R.id.tv_range_number);
         skDistance.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -66,5 +73,40 @@ public class UserRegisterActivity extends AppCompatActivity {
         Gallery ga = (Gallery) findViewById(R.id.gallery);
         ga.setAdapter(new ImageAdapter(this, imgs));
 
+        /*    PICKUP IMAGE   */
+        FloatingActionButton myFab = (FloatingActionButton) findViewById(R.id.fab);
+        myFab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    InputStream imageStream = null;
+                    try {
+                        imageStream = getContentResolver().openInputStream(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
+
+                    ImageSwitcher is = (ImageSwitcher) findViewById(R.id.is_user);
+
+                    Drawable drawable =new BitmapDrawable(yourSelectedImage);
+                    is.setImageDrawable(drawable);
+
+                }
+        }
+    }
+
 }
